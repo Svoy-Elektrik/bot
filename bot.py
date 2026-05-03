0862093d2a612f9d10246251a84b6ae5d560d023
Svoy-Elektrik <149778749+Svoy-Elektrik@users.noreply.github.com>
2026-05-03 11:27:21 +0200

    Fix formatting and whitespace in bot.py

--- bot.py
+++ bot.py
@@ -9,16 +9,14 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 async def main():
-    bot = Bot(token=os.getenv("BOT_TOKEN")) 
+    bot = Bot(token=os.getenv("BOT_TOKEN"))
     dp = Dispatcher(storage=MemoryStorage())
-
     dp.include_router(start.router)
     dp.include_router(seo.router)
     dp.include_router(image.router)
     dp.include_router(wordpress.router)
     dp.include_router(plagiat.router)
     dp.include_router(channel.router)
-
     logger.info("Bot started!")
     await dp.start_polling(bot)
 
