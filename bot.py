b2adc269ca9d5e09478953d4761a4ebfe3cf759b
Svoy-Elektrik <149778749+Svoy-Elektrik@users.noreply.github.com>
2026-05-04 21:41:18 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 async def main():
+    
     bot = Bot(token=os.getenv("BOT_TOKEN"))
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(start.router)
